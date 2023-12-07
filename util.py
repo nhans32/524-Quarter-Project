6e877db81c7c5c12eae480f2b3c3960ec443ed56
Nick H. <nhans3256@gmail.com>
2023-12-06 22:14:22 -0800

    general file cleanup

--- util.py
+++ util.py
@@ -1,19 +1,30 @@
-CLEAN_HEADER = """
-Subject: {subject}
-Message-ID: <GTUBE1.1010101@example.net>
-Date: Wed, 23 Jul 2003 23:30:00 +0200
-From: Sender <sender@example.net>
-To: Recipient <recipient@example.net>
-MIME-Version: 1.0
-Content-Type: text/plain; charset=us-ascii
-Content-Transfer-Encoding: 7bit
-"""
+from spamassassin_client import SpamAssassin
+from sklearn import metrics
 
-"""
-Add a header to any text file.
-"""
-def add_clean_header(subject, body):
-    pass
+def run_sa(inputs, sa_lvl=3): # sa_lvl defined as 3 because problem model doesnt consider headers
+    predictions = []
+    actual = []
+    records_corr_pred = []
 
-def clean_data():
-    pass
+    for idx, (text, label) in enumerate(inputs):
+        try:
+            sa = SpamAssassin(bytes(text, 'utf-8'))
+            pred = 1 if sa.is_spam(level=sa_lvl) else 0
+
+            predictions.append(pred)
+            actual.append(label)
+            if pred == label:
+                records_corr_pred.append((text, label))
+
+            print(f"{idx+1}/{len(inputs)}", end=' ')
+        except:
+            continue
+    print("\n")
+    return predictions, actual, records_corr_pred
+
+
+def evaluate(inputs, sa_lvl=3):
+    predictions, actual, records_corr_pred = run_sa(inputs, sa_lvl)
+    print(metrics.classification_report(actual, predictions))
+    print(metrics.confusion_matrix(actual, predictions))
+    return records_corr_pred
