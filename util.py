64a5e28bc456c1863d788829870a2b2a6a1c6597
Nick H. <nhans3256@gmail.com>
2023-12-06 16:15:03 -0800

    multiple edits, data inspection and testing loops

---
+++ util.py
@@ -0,0 +1,19 @@
+CLEAN_HEADER = """
+Subject: {subject}
+Message-ID: <GTUBE1.1010101@example.net>
+Date: Wed, 23 Jul 2003 23:30:00 +0200
+From: Sender <sender@example.net>
+To: Recipient <recipient@example.net>
+MIME-Version: 1.0
+Content-Type: text/plain; charset=us-ascii
+Content-Transfer-Encoding: 7bit
+"""
+
+"""
+Add a header to any text file.
+"""
+def add_clean_header(subject, body):
+    pass
+
+def clean_data():
+    pass
